046670c674b60c8e8eeb1987968b367fbdc84f04
Luke Climenhage <luke.climenhage@gmail.com>
2022-11-01 14:32:36 -0400

    Added auth middleware and protected routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const {
 	registerUser,
 	loginUser,
+	getMe,
 } = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
+// Second argument is for private (protected) routes
+router.get("/me", protect, getMe);
 
 module.exports = router;
